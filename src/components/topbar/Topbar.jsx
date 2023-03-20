49e78b60c765f7bc7edffdfee871d08794afcea6
david zambrano corral <Yayoamigo>
2023-03-20 15:53:51 -0500

    fix mui

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./topbar.css";
-import { NotificationsNone, Language, Settings } from "@material-ui/icons";
+import { NotificationsNone, Language, Settings } from "@mui/icons-material";
 
 export default function Topbar() {
   return (
